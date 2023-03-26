a12e82d23c393363253327cd84d41f02b06812f4
agengemb <32024540+UhtredTheDane@users.noreply.github.com>
2023-03-26 04:36:37 +0200

    merge master luca_final finalisé

--- includes/builtins.h
+++ includes/builtins.h
@@ -41,20 +41,6 @@ int    builtin_unset(t_parse *p, t_cmd *cmd);
 
 
 int is_pwd(t_cmd *cmd);
-<<<<<<< HEAD
-=======
-int is_cd(t_cmd *cmd);
-int is_unset(t_parse *p, t_cmd *cmd);
-int is_export(t_parse *p, t_cmd *cmd);
-int is_env(t_parse *p, t_cmd *cmd);
-int is_exit(t_parse *p, t_cmd *cmd);
-void    builtin_env(char **envp);
-void    builtin_exit(t_envp *envp, int return_value);
-int    builtin_export(t_envp *envp, char *key, char *value);
-void    builtin_echo(char **messages, int n_option);
-void builtin_unset(t_envp **envp, char *key);
-int builtin_cd(t_envp *envp, const char *path);
->>>>>>> origin/luca_final
 char *builtin_pwd();
 void    print_pwd();
 
