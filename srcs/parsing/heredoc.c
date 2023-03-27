b8279f8fbf68cb2a82b309b718ad7a7efc209287
agengemb <32024540+UhtredTheDane@users.noreply.github.com>
2023-03-27 23:45:12 +0200

    heredoc fini

--- srcs/parsing/heredoc.c
+++ srcs/parsing/heredoc.c
@@ -86,7 +86,6 @@ int get_heredoc(t_parse *p, t_cmd* cmd, char *word)
 {
 	pid_t pid;
 	int status; 
-	int return_code;
 
 	p = (t_parse *) p;
 	cmd->pipe_heredoc = malloc(sizeof(int) * 2);
@@ -111,11 +110,12 @@ int get_heredoc(t_parse *p, t_cmd* cmd, char *word)
 	if (!update_sigint_interactive(0))
 		return (0);
 	waitpid(-1, &status, 0);
-	if (WIFEXITED(status))
-		cmd_return = WEXITSTATUS(status);
 	if (!update_sigint_no_interactive()) 
 		return (1);
 	close(cmd->pipe_heredoc[1]);
+	if (WIFEXITED(status))
+		if (!WEXITSTATUS(status))
+			return (0);
 	return (1);	
 }
 
