9e8f1c89561141fa157beda9c2a5476b06ede25d
agengemb <gengembre.alexis@gmail.com>
2023-04-06 16:53:06 +0200

    compil ok saignaux 3

--- includes/main.h
+++ includes/main.h
@@ -6,7 +6,7 @@
 /*   By: agengemb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 17:10:03 by agengemb          #+#    #+#             */
-/*   Updated: 2023/03/27 18:02:44 by lloisel          ###   ########.fr       */
+/*   Updated: 2023/04/06 16:51:46 by agengemb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,8 @@
 # include "pipex.h"
 
 typedef struct s_dico t_dico;
+typedef struct s_parse t_parse;
+typedef struct s_envp t_envp;
 int clean_exit(t_parse *p,t_envp *envp);
 void	son_proc(char *input_cmd, char **envp, int *pipe_fd, size_t num_proc);
 #endif
